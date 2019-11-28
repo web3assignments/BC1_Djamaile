4d5af46011e24ab3b3e6de22ea7fa65a48aeb108
djamaile <djamaile.rahamat@minbzk.nl>
2019-11-28 23:10:48 +0100

    new calorie contract

---
+++ Day_04_Wed_27_11_2019/Calorie.sol
@@ -0,0 +1,118 @@
+pragma solidity ^0.5.11;
+
+import "./BokkyPooBahsDateTimeLibrary.sol";
+
+contract Calorie {
+
+    event NewFoodItem(string message, uint id, string foodName, uint cal);
+    event calorieBurned(string message, uint cal);
+    event checkedCalByDate(string message, uint dateSearched);
+    event paidUser(string message, uint paid, uint userBalance);
+
+    uint public eatenCal;
+    uint public burnedCal;
+    uint public totalCal = eatenCal - burnedCal;
+    uint public dailyLimit = 1900;
+    address payable wallet;
+
+    struct FoodItem {
+        address User;
+        string foodName;
+        uint cal;
+        uint dateReg;
+    }
+
+    FoodItem[] internal foodItems;
+    mapping(address => FoodItem) foodItemMap;
+    mapping(bytes32 => uint) totalCaloriesByDate;
+    mapping(address => uint) userBalance;
+
+    constructor() public payable {
+        wallet = msg.sender;
+    }
+
+    function _addFoodItem(string memory _foodName, uint _cal) private {
+        bytes memory _foodNameB = bytes(_foodName);
+
+        require(_foodNameB.length > 1, "Did not receive the name of the food");
+        require(_cal > 0, "Did not receive the calorie of the food");
+
+        uint _dateReg = now;
+        FoodItem memory foodItem = FoodItem(msg.sender,_foodName, _cal, _dateReg);
+        uint id = foodItems.push(foodItem) - 1;
+        foodItemMap[msg.sender] = foodItem;
+        (uint year, uint month, uint day) = timestampToDate(_dateReg);
+        bytes32 dateFormatedToBytes = keccak256(abi.encodePacked(year + month + day));
+        totalCaloriesByDate[dateFormatedToBytes] += _cal;
+        totalCal += _cal;
+        eatenCal += _cal;
+        emit NewFoodItem("New Food item added", id, _foodName, _cal);
+    }
+
+    function addFoodItem(string memory _foodName, uint _cal) public {
+        _addFoodItem(_foodName, _cal);
+    }
+
+
+    function _getTotalCalEatenByDate(uint _date) private view returns (uint) {
+        require(foodItems.length > 0, "You have no food item added");
+        uint _totalCal = 0;
+        (uint searchedYear, uint searchedMonth, uint searchedDay ) = timestampToDate(_date);
+        bytes32 searchedDate = keccak256(abi.encodePacked(searchedYear + searchedMonth + searchedDay));
+        _totalCal = totalCaloriesByDate[searchedDate];
+        require(_totalCal > 0, "There were no foods added with this date");
+        return _totalCal;
+    }
+
+    function getTotalCalEatenByDate(uint year, uint month, uint day)public view returns (uint){
+        uint unixTimeStamp = timestampFromDate(year,month,day);
+        return _getTotalCalEatenByDate(unixTimeStamp);
+    }
+
+    function timestampFromDate(uint year, uint month, uint day) private pure returns (uint timestamp) {
+        return BokkyPooBahsDateTimeLibrary.timestampFromDate(year, month, day);
+    }
+
+    function timestampToDate(uint timestamp) private pure returns (uint year, uint month, uint day) {
+        (year, month, day) = BokkyPooBahsDateTimeLibrary.timestampToDate(timestamp);
+    }
+
+    function burnCal(uint _cal) public {
+        burnedCal += _cal;
+        totalCal -= _cal;
+        emit calorieBurned("We burned some calories", _cal);
+    }
+
+    function getFoodItemFromUser() public view returns (address, string memory, uint, uint){
+        require(foodItemMap[msg.sender].cal > 1, "User does not exit in our program");
+        FoodItem memory item = foodItemMap[msg.sender];
+        return (item.User, item.foodName, item.cal, item.dateReg);
+    }
+
+    /*
+     The idea is that a user will get tokens based on his burned calories - eaten calories that is over the daily limit.
+     For example, John has daily limit of 1900 calories he can eat per day. He burns 400 calories that day and is still
+     within his limit of 1900 calories, he will get paid 400 calorie tokens. But if John ate 2100 calories and burned 400,
+     he would only get 200 calorie tokens.
+
+     This is still a idea in progress.
+    */
+    function payOutTheUser() public payable {
+        uint calDebt = 0;
+
+        if(eatenCal > dailyLimit){
+            calDebt = dailyLimit - eatenCal;
+        }
+
+        //require(burnedCal < calDebt, "You need to burn more calories before you can get paid!");
+
+        uint userGotPaid = burnedCal - calDebt;
+        wallet.transfer(userGotPaid);
+        totalCal = 0;
+        emit paidUser("paid the user: ", userGotPaid, getUserBalance());
+    }
+
+    function getUserBalance() public view returns (uint){
+        return wallet.balance;
+    }
+}
--- Day_04_Wed_27_11_2019/callContract.js
+++ Day_04_Wed_27_11_2019/callContract.js
@@ -15,16 +15,73 @@ const TestPayABI = [
         "payable": false,
         "stateMutability": "view",
         "type": "function"
-    }
+    },
+    {
+        "constant": true,
+        "inputs": [],
+        "name": "getUserBalance",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": false,
+        "inputs": [
+            {
+                "internalType": "uint256",
+                "name": "_cal",
+                "type": "uint256"
+            }
+        ],
+        "name": "burnCal",
+        "outputs": [],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
 ];
 
 async function getTotalCal() {
     const ContractAddress = "0xB7005405101E61fcD81ec1322AE72c54a99AED7A";
     const CreateContract = new web3.eth.Contract(TestPayABI, ContractAddress);
-    
+
     let totalCal = await CreateContract.methods.totalCal().call();
 
     console.log(`User has ${totalCal} total calories consumed`);
 }
 
-getTotalCal();
+async function burnCalories(){
+    const privateKey = 'e0f3440344e4814d0dea8a65c1b9c488bab4295571c72fb879f5c29c8c861937';
+    const account = web3.eth.accounts.privateKeyToAccount('0x' + privateKey);
+    web3.eth.accounts.wallet.add(account);
+    web3.eth.defaultAccount = account.address;
+
+    const ContractAddress = "0x288191b6e0B4BCC6Baba6620B60E20871ae629c2";
+    const CreateContract = new web3.eth.Contract(TestPayABI, ContractAddress);
+
+
+     const burned = await CreateContract.methods.burnCal(100).send({from: this.web3.eth.defaultAccount,
+         gas:1,
+         gasPrice: 1});
+     console.log(burned);
+}
+
+async function getUserBalance(){
+    const ContractAddress = "0x288191b6e0B4BCC6Baba6620B60E20871ae629c2";
+    const CreateContract = new web3.eth.Contract(TestPayABI, ContractAddress);
+
+    let balance = await CreateContract.methods.getUserBalance().call();
+    balance = parseInt(balance);
+    console.log(`User has ${balance} ether`);
+}
+
+//getTotalCal();
+//getUserBalance();
+burnCalories();
